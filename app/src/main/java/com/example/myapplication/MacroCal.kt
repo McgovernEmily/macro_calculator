ba76f23542d2e7f46eee044c3077d246a6d91d75
Emily McGovern <101mcgoverne@gmail.com>
2026-01-15 21:42:31 -0700

    Added logic and started design

---
+++ app/src/main/java/com/example/myapplication/MacroCal.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication
+
+import kotlin.math.roundToInt
+
+data class Macros(
+    val calories: Int,
+    val protein: Int,
+    val carbs: Int,
+    val fats: Int
+)
+
+fun calculate(
+    weightkg: Double,
+    heightcm: Double,
+    age: Int,
+    sex: String): Macros {
+
+    // Calculating the calories.
+    val bmr = if (sex.lowercase() == "male") {
+        10 * weightkg + 6.25 * heightcm - 5 * age + 5
+    }else{
+        10 * weightkg +6.25 * heightcm - 5 * age - 161
+    }
+
+
+    val finalcalories = (bmr * 1.2).roundToInt()
+
+    val protein = (0.8 * weightkg).roundToInt()
+
+    val fat = ((0.3 * finalcalories)/ 9).roundToInt()
+
+    val carbs = ((0.4 * finalcalories)/ 4).roundToInt()
+
+    return Macros(calories = finalcalories, protein = protein, fats = fat, carbs = carbs)
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,36 +14,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            setContentView(R.layout.activty_main)
         }
     }
-}
 
-@Composable
-fun
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
 }
