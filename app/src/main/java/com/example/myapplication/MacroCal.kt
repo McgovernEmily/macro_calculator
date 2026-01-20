b0d1c515b229ec2a0e61c5773a12dc5ed57b9ee6
Emily McGovern <101mcgoverne@gmail.com>
2026-01-19 21:32:33 -0700

    Fixed for levels of activity

--- app/src/main/java/com/example/myapplication/MacroCal.kt
+++ app/src/main/java/com/example/myapplication/MacroCal.kt
@@ -8,7 +8,7 @@ data class Macros(
     val protein: Int,
     val carbs: Int,
     val fats: Int,
-    val activitylevel
+    val activitylevel: Double
 
 )
 
@@ -17,7 +17,7 @@ fun calculate(
     heightcm: Double,
     age: Int,
     sex: String,
-    activitylevel: Double): Macros {
+    activitylevel: Int): Macros {
 
     // Calculating the calories.
     val bmr = if (sex.lowercase() == "male") {
@@ -26,9 +26,15 @@ fun calculate(
         10 * weightkg +6.25 * heightcm - 5 * age - 161
     }
 
-    val activitySel = if(activitylevel)
+    // 1 = sedentary, 2 = light, 3 = moderate, 4 = very active, 5 = super active
+    val levels = mapOf(1 to 1.2,
+        2 to 1.375, 3 to 1.55,
+        4 to 1.725, 5 to 1.9)
 
-    val finalcalories = (bmr * 1.2).roundToInt()
+    // It will grab the levels, but if there is none then it will do lower end.
+    val multiplier = levels[activitylevel] ?: 1.2
+
+    val finalcalories = (bmr * multiplier).roundToInt()
 
     val protein = (0.8 * weightkg).roundToInt()
 
@@ -36,5 +42,5 @@ fun calculate(
 
     val carbs = ((0.4 * finalcalories)/ 4).roundToInt()
 
-    return Macros(calories = finalcalories, protein = protein, fats = fat, carbs = carbs)
+    return Macros(calories = finalcalories, protein = protein, fats = fat, carbs = carbs, activitylevel = multiplier)
 }
